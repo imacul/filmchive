d7bed4aee5f184028f2051dc0ba0a2438e924dc6
Emmanuel oshakpemeh <149076550+imacul@users.noreply.github.com>
2025-03-27 05:32:52 +0000

    add analytics, solve feedback

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import FeedbackForm from "@/components/FeedbackForm";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
         {children}
         <FeedbackForm />
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
